ca03d8007e29affbd7fcb51aa06818c481cbbd67
SanchoKusika <99037228+SanchoKusika@users.noreply.github.com>
2024-01-23 20:40:32 +0300

    Add files via upload

---
+++ c++/lab3/lab3/lab3.cpp
@@ -0,0 +1,144 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <locale.h>
+
+
+template <typename T>
+class Vect {
+public:
+    T* data;
+    size_t size;
+    size_t capacity;
+
+    void resize(size_t new_capacity) {
+        T* temp = new T[new_capacity];
+        for (size_t i = 0; i < size; ++i) {
+            temp[i] = data[i];
+        }
+        delete[] data;
+        data = temp;
+        capacity = new_capacity;
+    }
+
+public:
+    Vect() : data(nullptr), size(0), capacity(0) {}
+
+    ~Vect() {
+        delete[] data;
+    }
+
+    void push_back(const T& value) {
+        if (size == capacity) {
+            resize(capacity == 0 ? 1 : capacity * 2);
+        }
+        data[size++] = value;
+    }
+
+    T& operator[](size_t index) {
+        setlocale(LC_ALL, "Russian");
+        if (index >= size) {
+            throw std::out_of_range("Индекс выходит за границы массива");
+        }
+        return data[index];
+    }
+
+    // Константная перегрузка оператора []
+    const T& operator[](size_t index) const {
+        setlocale(LC_ALL, "Russian");
+        if (index >= size) {
+            throw std::out_of_range("Индекс выходит за границы массива");
+        }
+        return data[index];
+    }
+
+    size_t getSize() const {
+        return size;
+    }
+
+    void print() const {
+        if (size == 0) {
+            std::cout << "[]";
+        }
+        else {
+            for (size_t i = 0; i < size; ++i) {
+                std::cout << "[" << data[i] << "] ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    void pop_back() {
+        if (size == 0) {
+            throw std::out_of_range("Попытка удалить элемент из пустого массива");
+        }
+        size--;
+    }
+};
+
+
+template <typename T>
+class Stack {
+private:
+    Vect<T> vect;
+
+public:
+    void push(const T& value) {
+        vect.push_back(value);
+    }
+
+    T pop() {
+        if (vect.getSize() == 0) {
+            setlocale(LC_ALL, "Russian");
+            throw std::underflow_error("Стек пуст");
+        }
+        T value = vect[vect.getSize() - 1];
+        vect.pop_back();
+        return value;
+    }
+
+    T top() const {
+        if (vect.getSize() == 0) {
+            setlocale(LC_ALL, "Russian");
+            throw std::underflow_error("Стек пуст");
+        }
+        return vect[vect.getSize() - 1];
+    }
+
+    bool isEmpty() const {
+        return vect.getSize() == 0;
+    }
+
+    void print() const {
+        vect.print();
+    }
+
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    try {
+        Stack<int> intStack;
+
+        intStack.push(10);
+        intStack.push(20);
+        intStack.push(30);
+
+        std::cout << "Содержимое стека до изменений: ";
+        intStack.print();
+
+        std::cout << "Удаленный элемент: " << intStack.pop() << std::endl;
+        std::cout << "Удаленный элемент: " << intStack.pop() << std::endl;
+        std::cout << "Удаленный элемент: " << intStack.pop() << std::endl;
+        
+        std::cout << "Содержимое стека после изменений: ";
+        intStack.print();
+
+        intStack.pop();
+    }
+
+    catch (const std::exception& e) {
+        std::cerr << "Исключение: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ c++/lab4/lab4/lab4.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <locale.h>
+
+class AEROFLOT {
+private:
+    std::string destination;
+    int flightNumber;
+    std::string planeType;
+
+public:
+    AEROFLOT() : destination(""), flightNumber(0), planeType("") {}
+    AEROFLOT(std::string dest, int number, std::string type)
+        : destination(dest), flightNumber(number), planeType(type) {}
+
+    void setDestination(const std::string& dest) { destination = dest; }
+    void setFlightNumber(int number) { flightNumber = number; }
+    void setPlaneType(const std::string& type) { planeType = type; }
+
+    std::string getDestination() const { return destination; }
+    int getFlightNumber() const { return flightNumber; }
+    std::string getPlaneType() const { return planeType; }
+
+    // Перегрузка операторов
+    friend std::istream& operator>>(std::istream& in, AEROFLOT& a);
+    friend std::ostream& operator<<(std::ostream& out, const AEROFLOT& a);
+};
+
+std::istream& operator>>(std::istream& in, AEROFLOT& a) {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Пункт назначения: ";
+    in >> a.destination;
+    std::cout << "Номер рейса: ";
+    in >> a.flightNumber;
+    std::cout << "Тип самолета: ";
+    in >> a.planeType;
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const AEROFLOT& a) {
+    setlocale(LC_ALL, "Russian");
+    out << "Номер рейса: " << a.flightNumber << ", Тип самолета: " << a.planeType
+        << ", Пункт назначения: " << a.destination;
+    return out;
+}
+
+bool compareFlightNumber(const AEROFLOT& a1, const AEROFLOT& a2) {
+    return a1.getFlightNumber() < a2.getFlightNumber();
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::vector<AEROFLOT> flights(7);
+    std::string searchDestination;
+
+    std::cout << "Введите данные о 7 рейсах (пункт назначения, номер рейса, тип самолета):\n";
+    for (int i = 0; i < 7; ++i) {
+        std::cout << "\n" << i + 1 << " рейс:\n";
+        std::cin >> flights[i];
+    }
+
+    std::sort(flights.begin(), flights.end(), compareFlightNumber);
+
+    std::cout << "\nВведите пункт назначения для поиска рейсов: ";
+    std::cin >> searchDestination;
+
+    bool found = false;
+    for (const auto& flight : flights) {
+        if (flight.getDestination() == searchDestination) {
+            std::cout << flight << std::endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        std::cout << "Рейсы в " << searchDestination << " не найдены." << std::endl;
+    }
+
+    return 0;
+}
